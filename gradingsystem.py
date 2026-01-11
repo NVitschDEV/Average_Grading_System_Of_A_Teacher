99898275c76d2ffe17f4b677e855ed7b2c7c0c4b
NVitschDEV <omar.hozdic@tutamail.com>
2026-01-11 23:15:09 +0100

    Update gradingsystem.py

--- gradingsystem.py
+++ gradingsystem.py
@@ -20,12 +20,44 @@ french_grades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 
 
 
 def grademaker():
-    s1 = input("What grading System would you like to use? ")
-    if s1 == 1:
-        print("Swiss System")
-        print("This is the Grade of your Student:")
-        grade_random = random(swiss_grades)
-        print(grade_random)
+    grades = []
+    number = 1
+
+    while number == 1:
+        s1 = input(
+            "What grading System would you like to use? 1. Swiss, 2. German, 3. American System, 4. French System "
+        )
+        print("'done' when you are done")
+        if s1 == "1":
+            print("Swiss System")
+            print("This is the Grade of your Student:")
+            grade_random = random.choice(swiss_grades)
+            print(grade_random)
+            grades.append(grade_random)
+        elif s1 == "2":
+            print("German System")
+            print("This is the Grade of your Student:")
+            grade_random = random.choice(german_grades)
+            print(grade_random)
+            grades.append(grade_random)
+        elif s1 == "3":
+            print("American System")
+            print("This is the Grade of your Student:")
+            grade_random = random.choice(american_grades)
+            print(grade_random)
+            grades.append(grade_random)
+        elif s1 == "4":
+            print("French System")
+            print("This is the Grade of your Student:")
+            grade_random = random.choice(french_grades)
+            print(grade_random)
+            grades.append(grade_random)
+        elif s1 == "done":
+            number = 2
+        else:
+            print("please enter something valid!")
+
+    print(f"Here are the realistic Grades: {grades}")
 
 
 grademaker()
